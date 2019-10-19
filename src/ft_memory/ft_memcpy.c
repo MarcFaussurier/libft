805e97be7f634ea7edfe66f42e233cd63440db51
Marc Faussurier <faussurier.marc@icloud.com>
2019-10-19 16:07:10 +0200

    memtest

---
+++ src/ft_memory/ft_memcpy.c
@@ -0,0 +1,18 @@
+#include <ft.h>
+
+void    *ft_memcpy(void *dest, const void *src, size_t n)
+{
+    unsigned char        *d;
+    const unsigned char  *s;
+    size_t              i;
+
+    i = 0;
+    d = (unsigned char*) dest;
+    s = (unsigned char*) src;
+    while(i < n)
+    {
+        d[i] = s[i];
+        i += 1;
+    }
+    return(dest);
+}
