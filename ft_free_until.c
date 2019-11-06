4a51f4b7be0a823b3c75667f09700c7391cf4215
Marc Faussurier <mfaussur@z2r2p1.le-101.fr>
2019-11-06 22:30:27 +0100

    ft_free_until

--- ft_free_until.c
+++ ft_free_until.c
@@ -6,7 +6,7 @@
 /*   By: mfaussur <mfaussur@student.le-101.>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/28 16:24:58 by mfaussur     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/29 02:52:26 by mfaussur    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/06 22:28:59 by mfaussur    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,9 +18,11 @@ void			ft_free_until(void **array, void *end)
 	void		*tmp;
 
 	tmp = array;
-	while (*array != end)
+	while (TRUE)
 	{
 		free(*array);
+		if (*array != end)
+			break;
 		array += 1;
 	}
 	free(tmp);
