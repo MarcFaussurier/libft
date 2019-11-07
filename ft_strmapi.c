3d9125985c534aeecfce056189e5e4e35fc3d797
Marc Faussurier <faussurier.marc@icloud.com>
2019-11-08 00:37:28 +0100

    loc

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -20,15 +20,12 @@ char				*ft_strmapi(const char *s, char (*f)(unsigned int, char))
 
 	if (!s || !f)
 		return (NULL);
-	i = 0;
 	out = malloc((ft_strlen(s) + 1) * sizeof(char));
 	if (!out)
 		return (NULL);
-	while (s[i])
-	{
+	i = -1;
+	while (s[++i])
 		out[i] = f(i, s[i]);
-		i += 1;
-	}
 	out[i] = '\0';
 	return (out);
 }
